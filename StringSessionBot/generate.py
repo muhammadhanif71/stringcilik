1fb0a1e46c073e76165d78f69539d222155dc031
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-14 20:28:09 +0700

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -22,7 +22,7 @@ from telethon.errors import (
 )
 
 ERROR_MESSAGE = "Oops! An exception occurred! \n\n**Error** : {} " \
-            "\n\nTolong Laporan ke [Support](t.me/CilikSupport) jika eror " \
+            "\n\nTolong Laporan ke [Support](t.me/stufsupport) jika eror " \
             "sensitive information and you if want to report this as " \
             "this error message is not being logged by us!"
 
